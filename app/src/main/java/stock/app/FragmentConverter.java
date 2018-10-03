36820a44087bd6cf1892ffc70aca3d0ecb87351e
komuvill <V-V.Komulainen@hotmail.com>
2018-10-03 14:41:53 +0300

    Valuuttamuuntimen runko

--- app/src/main/java/stock/app/FragmentConverter.java
+++ app/src/main/java/stock/app/FragmentConverter.java
@@ -11,9 +11,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FragmentConverter extends Fragment {
     private final String TAG = "Converter Fragment";
+
+    EditText fromCurrency;
+    EditText toCurrency;
+    EditText convertedAmount;
+    TextView conversionResults;
+    Button convertButton;
+    Button backButton;
+
 
     public FragmentConverter() {
         // Required empty public constructor
@@ -24,6 +35,19 @@ public class FragmentConverter extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_results, container, false);
         Log.d(TAG, "Started.");
+        fromCurrency = view.findViewById(R.id.editTextFromCurrency);
+        toCurrency = view.findViewById(R.id.editTextToCurrency);
+        convertedAmount = view.findViewById(R.id.editTextConvertedAmount);
+        conversionResults = view.findViewById(R.id.textViewConversionResult);
+
+        convertButton = view.findViewById(R.id.buttonConvert);
+        convertButton.setOnClickListener(v -> {
+
+        });
+        backButton = view.findViewById(R.id.buttonBack);
+        backButton.setOnClickListener(v -> {
+            ((MainActivity)getActivity()).setViewPager(MainActivity.FRAGMENT_MENU);
+        });
 
         return view;
     }
