986cbc9477776aa8a715db30576a0467ed3a599c
Miqu-el <miquelraisinghell@gmail.com>
2018-10-09 13:47:39 +0300

    temp

--- app/src/main/java/stock/app/FragmentResults.java
+++ app/src/main/java/stock/app/FragmentResults.java
@@ -4,8 +4,6 @@ package stock.app;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.os.Parcel;
-import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -16,27 +14,23 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.patriques.output.digitalcurrencies.IntraDay;
-import org.patriques.output.digitalcurrencies.data.SimpelDigitalCurrencyData;
-
-import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Objects;
 
 public class FragmentResults extends Fragment {
 
     private Button buttonGraph;
     private Button buttonBack;
-    private TextView shortCurrencyName;
+    private TextView shortName;
     private TextView textViewPrice;
-    private TextView longCurrencyName;
+    private TextView longName;
+    private boolean switchState;
     private String TAG = "FragmentResults";
-    private IntraDay data;
+    private org.patriques.output.digitalcurrencies.IntraDay cryptoData;
+    private org.patriques.output.timeseries.IntraDay stockData;
 
     /*
         Tällä hetkellä ulos saadaan tunnus ja hinta
      */
-
 
     //Called when the fragment becomes visible
     @Override
@@ -44,12 +38,18 @@ public class FragmentResults extends Fragment {
         super.setMenuVisibility(menuVisible);
         if(menuVisible){
             if(!getRetainInstance()){
-                data = ((MainActivity) Objects.requireNonNull(getActivity())).fetcher.getResult(); //haetaan data fetcheristä
+                switchState = getArguments().getBoolean("STATE");
+                if(switchState){
+                    stockData = ((MainActivity) Objects.requireNonNull(getActivity())).sFetcher.getResult();
+                    shortName.setText(stockData.getMetaData().get("2. Symbol"));
+                    textViewPrice.setText(String.format("%.2f", stockData.getStockData().get(0).getHigh()));
+                }else{
+                    cryptoData = ((MainActivity) Objects.requireNonNull(getActivity())).cFetcher.getResult();
+                    shortName.setText(cryptoData.getMetaData().get("2. Digital Currency Code"));
+                    longName.setText(cryptoData.getMetaData().get("3. Digital Currency Name"));
+                    textViewPrice.setText(String.format("%.2f", cryptoData.getDigitalData().get(0).getPriceA()));
+                }
             }
-
-            shortCurrencyName.setText(data.getMetaData().get("2. Digital Currency Code"));
-            longCurrencyName.setText(data.getMetaData().get("3. Digital Currency Name"));
-            textViewPrice.setText(String.format("%.2f", data.getDigitalData().get(0).getPriceA()));
         }
     }
 
@@ -61,21 +61,21 @@ public class FragmentResults extends Fragment {
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         buttonGraph = view.findViewById(R.id.buttonShowGraph);
         buttonBack = view.findViewById(R.id.buttonBackToSearch);
-        shortCurrencyName = view.findViewById(R.id.textViewCurNameShort);
+        shortName = view.findViewById(R.id.textViewCurNameShort);
         textViewPrice = view.findViewById(R.id.textViewPrice);
-        longCurrencyName = view.findViewById(R.id.textViewCurNameLong);
+        longName = view.findViewById(R.id.textViewCurNameLong);
 
         buttonGraph.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO Graph view
                 setRetainInstance(true);
-                int length = Math.min(data.getDigitalData().size(), 289);
+                int length = Math.min(cryptoData.getDigitalData().size(), 289);
                 double[] priceList = new double[length];
                 String[] timeList = new String[length];
                 for(int i = 0; i < length; i++){
-                    priceList[i] = data.getDigitalData().get(length - i - 1).getPriceA();
-                    timeList[i] = data.getDigitalData().get(length - i - 1).getDateTime().toString();
+                    priceList[i] = cryptoData.getDigitalData().get(length - i - 1).getPriceA();
+                    timeList[i] = cryptoData.getDigitalData().get(length - i - 1).getDateTime().toString();
                 }
                 Intent intent = new Intent(getActivity(),FragmentGraph.class);
                 intent.putExtra("priceData", priceList);
--- app/src/main/java/stock/app/MainActivity.java
+++ app/src/main/java/stock/app/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentSearch.switchStateInterface {
 
     private SectionsStatePagerAdapter adapter;
     private CustomPager viewPager;
@@ -14,8 +14,9 @@ public class MainActivity extends AppCompatActivity {
     public static final int FRAGMENT_RESULTS = 2;
     public static final int FRAGMENT_GRAPH = 3;
     public static final int FRAGMENT_CONVERTER = 4;
-    public CryptoFetcher fetcher;
+    public CryptoFetcher cFetcher;
     public CurrencyConverter converter;
+    public StockFetcher sFetcher;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,5 +36,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void setViewPager(int fragmentNumber){
         runOnUiThread(() -> viewPager.setCurrentItem(fragmentNumber));
+    }
+
+    @Override
+    public void onRequestSent(boolean state) {
+        Bundle bundle = new Bundle();
+        bundle.putBoolean("STATE", state);
+        adapter.getItem(2).setArguments(bundle);
     }
 }
